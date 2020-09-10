a5417dfdc3c98a97a2194fa207b898cec7665410
maikgiordano98 <70980802+maikgiordano98@users.noreply.github.com>
2020-09-10 18:52:44 -0300

    as

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ var imagenes=new Array(
         var index=Math.floor((Math.random()*imagenes.length));
  
         // cambiamos la imagen
-        document.getElementById("home").style.backgroundImage = "url("+[index]+")";
+        document.getElementById("home").style.backgroundImage = 'url('+imagenes[index]+')';
     }
  
     /**
